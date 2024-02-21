2cb6d2e1f968e3557a3fc39cb281fc1aa610ffba
hemaltank <155123835+hemaltank@users.noreply.github.com>
2024-02-21 10:56:43 +0530

    Delete script_6.php

--- script_6.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "<h2><b><font color=red>Insteoduction to Web Development</font></b></h2>";
-echo "<i>Web development is the process of creating website and web application that users can access through web browsers.</i>";
-?>
